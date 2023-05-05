6c903038b18110a20834b4966f92ce0c06c6dfb0
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-05 10:58:00 -0700

    implemented PUT method to updated users

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -29,15 +29,15 @@ router.post("/", validateUser, (req, res, next) => {
       res.status(201).json(user)
     })
     .catch(next)
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
 });
 
-router.put("/:id", validateUserId, (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
-  console.log(req.user);
+router.put("/:id", validateUserId, validateUser, (req, res, next) => {
+  console.log("==>", req.body);
+  User.update(req.params.id, req.body)
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(next)
 });
 
 router.delete("/:id", validateUserId, (req, res) => {
